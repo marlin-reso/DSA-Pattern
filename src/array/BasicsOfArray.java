01515fede90565b663348474a64230a1d78b364a
sanjay giri <sanju.reso@gmail.com git config --global user.name marlin-reso>
2025-10-19 07:29:39 +0530

    test

---
+++ src/array/BasicsOfArray.java
@@ -0,0 +1,24 @@
+package array;
+
+public class BasicsOfArray {
+
+	public static void main(String[] args) {
+		
+		
+		int value[]= {1,2,3,4,5,6,7};
+		
+		char words[]= {'a','b','c','d','e','f'};
+		
+		for(int numbers : value) {
+			
+			System.out.println(numbers);
+		}
+		
+		for(char letter: words) {
+			
+			System.out.println(letter);
+		}
+
+	}
+
+}
